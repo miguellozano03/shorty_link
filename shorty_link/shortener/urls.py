adabb2ba27d17a3f64f769a68f451c2033fbc08d
Miguel L <miguel.lozano.vergara@gmail.com>
2025-08-28 11:55:23 -0500

    urls and views created

--- shorty_link/shortener/urls.py
+++ shorty_link/shortener/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('shorten/', views.url_shortener_view, name='shorten_link')
+    path('generate_short/', views.generate_short, name='shorten_link'),
     
+    path('<str:short_code>/', views.redirect_url, name='redirect_url')
 ]
