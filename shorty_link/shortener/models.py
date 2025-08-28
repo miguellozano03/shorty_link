9b20b4a46f6d49c3b24d287aa40203a2f5c359d4
Miguel L <miguel.lozano.vergara@gmail.com>
2025-08-28 11:56:36 -0500

    feat: add database model

--- shorty_link/shortener/models.py
+++ shorty_link/shortener/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-class Urls(models.Model):
+class Url(models.Model):
     long_url = models.URLField()
     short_url  = models.CharField(max_length=20)
