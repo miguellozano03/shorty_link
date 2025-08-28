d33ffbf506825885328db62eaf00cbac597f8a3a
Miguel L <miguel.lozano.vergara@gmail.com>
2025-08-28 11:54:14 -0500

    feat: implement URL shortening

--- shorty_link/shortener/utils.py
+++ shorty_link/shortener/utils.py
@@ -1,9 +1,21 @@
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
 import base62
 import uuid
 
 def generate_short_code():
     random_uiid = uuid.uuid4().int
-
     shorten_url = base62.encode(random_uiid)
-
     return shorten_url[:7]
+
+def build_short_url(code, prefix: str = 'localhost:8000'):
+    return f'http://{prefix}/{code}'
+
+def valitate_url(url):
+    validate = URLValidator()
+    try:
+        validate(url)
+        return True
+    except ValidationError:
+        return False
--- shorty_link/shortener/views.py
+++ shorty_link/shortener/views.py
@@ -1,13 +1,32 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-from .utils import generate_short_code
-
-# Create your views here.
-
-def url_shortener_view(request):
-    code = generate_short_code()
-    return JsonResponse({"short_url": f"http:/shorty.link/{code}"})
-
+from .models import Url
+from django.http import JsonResponse, Http404
+from django.shortcuts import render, redirect
+from .utils import build_short_url,generate_short_code , valitate_url
 
 def home_view(request):
     return render(request, "shortener/home.html")
+
+def generate_short(request):
+    if request.method == 'GET':
+        long_url = request.GET.get('long_url')
+
+        if not long_url:
+            return JsonResponse({"error": "No URL provided"}, status=400)
+    
+        if not valitate_url(long_url):
+            return JsonResponse({"error": "The URL is invalid"}, status=400)
+        
+        code = generate_short_code()
+        while Url.objects.filter(short_url=code).exists():
+            code = generate_short_code()
+
+        Url.objects.create(long_url=long_url, short_url=code)
+        return JsonResponse({"short_url": build_short_url(code)})
+    return JsonResponse({"error": "Method not allowed"}, status=405)
+
+
+def redirect_url(request, short_code):
+    original = Url.objects.filter(short_url = short_code).first()
+    if original:
+        return redirect(original.long_url)
+    raise Http404("Short URL not found")
